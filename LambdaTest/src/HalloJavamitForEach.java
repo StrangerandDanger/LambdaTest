456ba3c719debcbab1a8b6ed7a9a773d84eb58f5
Simon OBERHUMER <OberhumerS22063@HTL.GRIESKIRCHEN.LOCAL>
2024-09-24 12:25:02 +0200

    assignment2 added and implemented

--- LambdaTest/src/HalloJavamitForEach.java
+++ LambdaTest/src/HalloJavamitForEach.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HalloJavamitForEach
 {
-    
+    public static void assignment2()
+    {
+        List<String> stringList = (() -> )
+        for (int i = 0; i < 10; i++)
+        {
+            stringList.add("HelloJava" + i);
+        }
+        for(String s:stringList)
+        {
+            System.out.println(s);
+        }
+        stringList.forEach(System.out :: print);
+    }
+
+
 }
